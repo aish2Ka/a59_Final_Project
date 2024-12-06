2b3ddf741e90efa22783b1319dd84726f4a4b6c0
HarshithKumarL <aishwarya.1si18ei003@gmail.com>
2024-12-06 11:02:54 +0530

    deployment commit

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,12 @@ const loginRoutes = require("./routes/loginRouter");
 const chartRoutes = require("./routes/chartRouter");
 const authenticateToken = require("./middleware/authMiddleware");
 const cors = require("cors");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 const app = express();
-
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    credentials: true,
+    origin: "http://localhost",
   })
 );
 
